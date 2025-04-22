dced198c4b2b98378931a51f21b469dbb28a9fda
Wesley Medeiros <devwesleymedeiros@yahoo.com>
2025-04-22 00:03:28 -0300

    Criação da home, ajuste nos formulários como troca de layout, criação do avatar de login

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -4,11 +4,31 @@ import { auth } from "@scripts/firebaseConfig";
 import { signOut } from "firebase/auth";
 
 const btnLogout = document.getElementById("logout");
+const inputAvatarAdd = document.getElementById("avatar-input");
+const imgAvatar = document.getElementById("avatar-image");
 
 document.addEventListener("DOMContentLoaded", () => {
+    // capiturar o link da imagem do usuário
+    const urlParams = new URLSearchParams(window.location.search);
+    const avatarUrl = urlParams.get("photo");
+    if (imgAvatar && avatarUrl) {
+        imgAvatar.src = avatarUrl;
+    }
+
+    inputAvatarAdd.addEventListener("change", () => {
+        const file = inputAvatarAdd.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                imgAvatar.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
     btnLogout.addEventListener("click", () => {
         signOut(auth).then(() => {
-            window.location.href = "/";
+        window.location.href = "/";
         }).catch((error) => {
             if (error.code === "auth/network-request-failed") {
                 alert("Network error. Por favor, verifique sua conexão com a internet.");
