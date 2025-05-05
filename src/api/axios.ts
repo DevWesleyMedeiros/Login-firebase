9b3a6cb2bcc87a9c9085eb9bfa7cf530896216eb
Wesley Medeiros <devwesleymedeiros@yahoo.com>
2025-05-05 18:45:14 -0300

    .

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,19 +1,9 @@
-// consumir api
 import axios from "axios";
-import { db } from "@services/firebaseConfig";
-import { collection, query, where, getDocs } from "firebase/firestore";
+const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
 
-const inputSearchBooks: HTMLInputElement = document.getElementById(
-  "search"
-) as HTMLInputElement;
-const btnSearchBooks: HTMLButtonElement = document.getElementById(
-  "btn_search"
-) as HTMLButtonElement;
-
-const inputSearchBooksValue: string = inputSearchBooks.value;
-
-const getBooks = async (inputSearchBooksValue: string) => {
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${inputSearchBooksValue}&key=AIzaSyA2rQGEOybXiHvq5BP3M_wC8XjHTQ7Xl7Y`;
-  const response = await axios.get(url);
-  return response.data;
-};
+const api = axios.create({
+  baseURL: `https://www.googleapis.com/books/v1/`,
+  params: {
+    key: apiKey,
+  },
+});
